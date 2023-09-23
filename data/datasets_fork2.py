d2c3876d56edf1604e48847d18b3bc86398fb44d
demon2036 <1165137798@qq.com>
2023-09-23 22:16:12 +0800

    commit

--- data/datasets_fork2.py
+++ data/datasets_fork2.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
     #                            num_workers=0)
     #dl = test_cycle(dataset_root='/home/john/data/imagenet_train_shards', batch_size=1024, num_workers=8)
     # dl = test_cycle(dataset_root='/root/fused_bucket/data/imagenet_train_shards', batch_size=1024, num_workers=8)
-    dl = test_cycle(dataset_root='gs:/jtitor-eu/data/imagenet_train_shards/imagenet_train_shards-{00000..00012}.tar', batch_size=1024, num_workers=8)
+    dl = test_cycle(dataset_root='gs:/jtitor-eu/data/imagenet_train_shards/imagenet_train_shards-00000.tar', batch_size=1024, num_workers=8)
 
     for _ in range(1000):
         # with tqdm(total=100000) as pbar:
