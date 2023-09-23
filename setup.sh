4d1d5a77e468edb64882638fb63e2395bb842bbd
demon2036 <1165137798@qq.com>
2023-09-23 15:39:50 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
+apt upgrade
+apt update
+apt install unrar rar
+apt-get -y --force-yes install golang
 pip install tensorflow tensorflow-datasets webdataset
--- test2.py
+++ test2.py
@@ -4,16 +4,17 @@ from torchvision.utils import save_image
 import einops
 import torch
 import numpy as np
+from tqdm import tqdm
 
 if __name__ == "__main__":
     dataset_builder = tfds.builder('imagenet2012',try_gcs=True,data_dir='gs://jtitor-eu/data/tensorflow_datasets')
-    ds=create_split(dataset_builder,batch_size=64,train=True)
+    ds=create_split(dataset_builder,batch_size=64,train=True,cache=True)
 
-    for data in ds:
+    for data in tqdm(ds):
         x,y=data['image'],data['label']
         print(x.shape)
-        x=np.array(x)
-        x=torch.Tensor(x)
-        x=einops.rearrange(x,'b h w c->b c h w')
-        save_image(x,'test.png')
+        # x=np.array(x)
+        # x=torch.Tensor(x)
+        # x=einops.rearrange(x,'b h w c->b c h w')
+        # save_image(x,'test.png')
         break
