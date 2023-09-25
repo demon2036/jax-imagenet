ee03c289d0c37a77234b5fa2b7b14865468fb22f
demon2036 <1165137798@qq.com>
2023-09-25 20:52:37 +0800

    commit

--- trainers/imagenet_trainer_tf.py
+++ trainers/imagenet_trainer_tf.py
@@ -104,10 +104,10 @@ def train_step(state: MyTrainState, batch):
 
 @partial(jax.pmap, axis_name='batch', )
 def eval_step(state, batch):
-    # variables = {'params': state.params, 'batch_stats': state.batch_stats}
-    # logits = state.apply_fn(variables, batch['image'], train=False, mutable=False)
+    variables = {'params': state.params, 'batch_stats': state.batch_stats}
+    logits = state.apply_fn(variables, batch['image'], train=False, mutable=False)
 
-    logits, new_model_state = state.apply_fn({'params': state.params}, batch['image'], mutable=['batch_stats'])
+    # logits, new_model_state = state.apply_fn({'params': state.params}, batch['image'], mutable=['batch_stats'])
     return compute_metrics(logits, batch['label'])
 
 
