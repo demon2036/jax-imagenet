93b2a3b091b9a2aa07d993e342eb57181bf6e7a5
demon2036 <1165137798@qq.com>
2023-09-27 18:56:09 +0800

    commit

--- train_tf.py
+++ train_tf.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     # trainer.eval()
     # trainer.state = flax.jax_utils.unreplicate(trainer.state)
     # trainer.state = switch_to_deploy(trainer.state, config)
-    # trainer.state = flax.jax_utils.replicate(trainer.state)
+    trainer.state = flax.jax_utils.replicate(trainer.state)
     trainer.eval()
 
     trainer.train()
