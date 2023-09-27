d37bac3290c45775cbb3eead6595efcaaaedf43e
demon2036 <1165137798@qq.com>
2023-09-27 19:09:47 +0800

    commit

--- trainers/imagenet_trainer_tf.py
+++ trainers/imagenet_trainer_tf.py
@@ -292,7 +292,7 @@ class ImageNetTrainer(Trainer):
                     self.finished_steps += 1
                 print()
 
-                if (epoch + 1) % 10 == 0:
+                if (epoch + 1) % 1 == 0:
                     self.state = sync_batch_stats(self.state)
                     self.eval()
                     self.state = flax.jax_utils.unreplicate(self.state)
