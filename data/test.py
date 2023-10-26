654e685fc0769f27fb1c6e04f8a1a654e34c22bb
demon2036 <1165137798@qq.com>
2023-10-26 15:46:34 +0800

    commit

--- data/test.py
+++ data/test.py
@@ -26,7 +26,7 @@ def test(x):
     x = A.Resize(224, 224)(image=x)['image']
     x=x/255
 
-    return {'images': x, 'labels': torch.nn.functional.one_hot(torch.Tensor(np.array(cls).reshape(-1)).to(torch.int64), 1000).float()}
+    return {'images': x, 'labels': torch.nn.functional.one_hot(torch.Tensor(np.array(cls).reshape(-1)).to(torch.int64), 1000)}
 
 
 def prepare_tf_data(xs):
@@ -53,7 +53,7 @@ def create_input_pipeline(*args,**kwargs):
     urls = 'pipe:gcloud alpha storage cat gs://luck-eu/data/imagenet_train_shards/imagenet_train_shards-{00073..00073}.tar '
     urls = 'pipe:gcloud alpha storage cat gs://luck-eu/data/imagenet_train_shards/imagenet_train_shards-{00000..00073}.tar '
 
-    # urls = 'pipe: cat /home/john/data/imagenet_train_shards/imagenet_train_shards-{00073..00073}.tar'
+    urls = 'pipe: cat /home/john/data/imagenet_train_shards/imagenet_train_shards-{00073..00073}.tar'
 
     def temp(x):
         del x['__key__']
@@ -79,4 +79,4 @@ if __name__=="__main__":
 
     data=next(dl)
     print(data['images'],)
-    print(data['labels'].dtype)
+    print(data['labels'])
