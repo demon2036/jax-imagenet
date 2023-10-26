15e09454b0a3aad26c7178e417ffb16e436639cf
demon2036 <1165137798@qq.com>
2023-10-26 14:20:27 +0800

    commit

--- data/datasets.py
+++ data/datasets.py
@@ -1,13 +1,12 @@
 import io
-from pathlib import Path
-import cv2
+
 import einops
 import jax
 import torch
 import tqdm
 import webdataset as wds
 import albumentations as A
-from torchvision.datasets import ImageFolder
+
 from torch.utils.data import DataLoader, Dataset, default_collate
 import numpy as np
 from torchvision.utils import save_image
@@ -17,29 +16,29 @@ from PIL import Image
 image_size = 224
 
 
-class ImagePreprocessor():
-    def __init__(self, image_size=224):
-        self.resize = A.Resize(image_size, image_size)
-        self.center_crop = A.CenterCrop(image_size, image_size, p=0.5)
-        self.random_horizontal_flip = A.HorizontalFlip()
-        self.normalize = A.Normalize(max_pixel_value=1)
-
-    def preprocess(self, img):
-        img = self.resize(image=img)['image']
-        # img = self.center_crop(image=img)['image']
-        img = self.random_horizontal_flip(image=img)['image']
-        img = self.normalize(image=img, max_pixel_value=1)['image']
-        return img
-
-
-def transfer_data(x):
-    # x = A.center_crop(x, image_size, image_size)
-    # x = A.resize(x, image_size, image_size, interpolation=cv2.INTER_CUBIC)
-    # A.normalize()
-    preprocessor = ImagePreprocessor(image_size=image_size)
-    x = preprocessor.preprocess(x)
-
-    return x
+# class ImagePreprocessor():
+#     def __init__(self, image_size=224):
+#         self.resize = A.Resize(image_size, image_size)
+#         self.center_crop = A.CenterCrop(image_size, image_size, p=0.5)
+#         self.random_horizontal_flip = A.HorizontalFlip()
+#         self.normalize = A.Normalize(max_pixel_value=1)
+#
+#     def preprocess(self, img):
+#         img = self.resize(image=img)['image']
+#         # img = self.center_crop(image=img)['image']
+#         img = self.random_horizontal_flip(image=img)['image']
+#         img = self.normalize(image=img, max_pixel_value=1)['image']
+#         return img
+#
+#
+# def transfer_data(x):
+#     # x = A.center_crop(x, image_size, image_size)
+#     # x = A.resize(x, image_size, image_size, interpolation=cv2.INTER_CUBIC)
+#     # A.normalize()
+#     preprocessor = ImagePreprocessor(image_size=image_size)
+#     x = preprocessor.preprocess(x)
+#
+#     return x
 
 
 """
@@ -62,20 +61,23 @@ def create_input_pipeline(dataset_root='./imagenet_train_shards', batch_size=128
 """
 
 
+def test(x):
+    cls = int(x['cls'].decode('utf-8'))
+    x = Image.open(io.BytesIO(x['jpg'])).convert('RGB')
+    x = np.array(x)
+    x = A.Resize(224, 224)(image=x)['image']
+    x = x / 255
+    return {'images': x,
+            'labels': torch.nn.functional.one_hot(torch.Tensor(np.array(cls).reshape(-1)).to(torch.int64), 1000)}
+
+
 def create_input_pipeline(dataset_root='./imagenet_train_shards', batch_size=1024, num_workers=8, pin_memory=True,
                           drop_last=True, shuffle_size=10000):
+
     urls = 'pipe:gcloud alpha storage cat gs://luck-eu/data/imagenet_train_shards/imagenet_train_shards-{00073..00073}.tar '
     urls = 'pipe:gcloud alpha storage cat gs://luck-eu/data/imagenet_train_shards/imagenet_train_shards-{00000..00073}.tar '
 
     # urls = 'pipe: cat /home/john/data/imagenet_train_shards/imagenet_train_shards-{00073..00073}.tar'
-    def test(x):
-        cls = int(x['cls'].decode('utf-8'))
-        x = Image.open(io.BytesIO(x['jpg'])).convert('RGB')
-        x = np.array(x)
-        x = A.Resize(224, 224)(image=x)['image']
-        x = x / 255
-        return {'images': x,
-                'labels': torch.nn.functional.one_hot(torch.Tensor(np.array(cls).reshape(-1)).to(torch.int64), 1000)}
 
     def temp(x):
         del x['__key__']
@@ -83,19 +85,17 @@ def create_input_pipeline(dataset_root='./imagenet_train_shards', batch_size=102
 
     dataset = wds.WebDataset(
         urls=urls,
-        shardshuffle=False).mcached().map(test).batched(batch_size, collation_fn=default_collate).map(temp)
+        shardshuffle=False).mcached().map(test).batched(1024, collation_fn=default_collate).map(temp)
 
-    dataloader = DataLoader(dataset, num_workers=96, prefetch_factor=4, batch_size=None, pin_memory=True,
-                            # drop_last=True,
+    dataloader = DataLoader(dataset, num_workers=64, prefetch_factor=4, batch_size=None,  # drop_last=True,
                             persistent_workers=True)
 
     while True:
         for _ in dataloader:
             yield _
 
-
 if __name__ == '__main__':
-
+    """"""
     # dl = create_input_pipeline(dataset_root='/root/fused_bucket/data/imagenet_train_shards', batch_size=32)
     dl = create_input_pipeline(dataset_root='/home/john/data/imagenet_train_shards', batch_size=32)
 
