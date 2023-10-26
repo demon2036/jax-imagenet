c260761b2153f6798545e67219978af2407f9b33
demon2036 <1165137798@qq.com>
2023-10-26 15:54:39 +0800

    commit

--- data/test.py
+++ data/test.py
@@ -24,7 +24,7 @@ def test(x):
     x = np.array(x)
     x = A.Resize(224, 224)(image=x)['image']
 
-    return {'images': x, 'labels': torch.nn.functional.one_hot(torch.Tensor(np.array(cls).reshape(-1)).to(torch.int64), 1000)}
+    return {'images': x, 'labels': torch.nn.functional.one_hot(torch.Tensor(np.array(cls).reshape(-1)).to(torch.int64), 1000).float()}
 
 
 def prepare_tf_data(xs):
