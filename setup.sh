31f6a188988981c1f1ce78d401586db078038f81
demon2036 <1165137798@qq.com>
2023-11-29 00:09:52 +0800

    commit

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ apt upgrade
 apt update
 apt install unrar rar
 sudo apt-get install golang
-#pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-#pip install flax --ignore-installed PyYAML
+pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install flax --ignore-installed PyYAML
 pip install jax_smi
 pip install tensorflow==2.13 tensorflow-datasets webdataset keras-cv tf-models-official==2.13.1  tensorflow_addons
 pip install albumentations einops tqdm matplotlib   timm
